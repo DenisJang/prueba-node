bb5f65f73f2636a6b9b8617f84afeb814b23413a
DenisJang <deniijang@gmail.com>
2021-09-07 22:46:40 -0300

    mensaje

---
+++ git.js
@@ -0,0 +1,9 @@
+  
+const getTable = (numero) => {
+    for (let i = 0; i <= 10; i++) {
+      console.log(` ${numero} * ${i} = ${numero * i}`);
+    }
+  };
+  getTable(7)
+  
+  console.log(`Como los quiero ❤️`);
